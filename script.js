169e1e05e0322c2aa0a4df0ff83f3a6130819a47
Justin <114303358+noobcoder42@users.noreply.github.com>
2023-11-27 21:36:05 +0000

    created the playRound function
    The playRound function wil be the overall mechanics of the game and will decide the winner of the game

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissor"];
+    const choices = ["rock", "paper", "scissors"];
     const randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 }
 
 function playerChoice() {
-    let playerInput = prompt("Choose between rock, paper, or scissor");
+    let playerInput = prompt("Choose between rock, paper, or scissors");
     return playerInput.toLowerCase();
 }
 
 let player = playerChoice();
 let computer = getComputerChoice();
 
-console.log(computer);
-console.log(player);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Player wins";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Player wins";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Player wins";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Computer wins";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Computer wins";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Computer wins";
+    } else {
+        return "It's a draw!";
+    }
+}
