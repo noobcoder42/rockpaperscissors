1a923aa4645be6f1871d03f03fe0445c56612d28
Justin <114303358+noobcoder42@users.noreply.github.com>
2023-11-27 22:29:45 +0000

    reworked on the playRound function
     I shortened the code using the || (OR) operator

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ let player = playerChoice();
 let computer = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "Player wins";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "Player wins";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Player wins";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "Computer wins";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "Computer wins";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "Computer wins";
-    } else {
-        return "It's a draw!";
-    }
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+            return "Player wins";
+        } else if ((playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors")) {
+            return "Computer wins";
+        } else if (playerSelection === computerSelection) {
+            return "It's a draw"; 
+        } else {
+            return "Please choose between rock, paper, or scissors"; 
+        }
 }
+
+
+
